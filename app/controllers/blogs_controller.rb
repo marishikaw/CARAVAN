02729a9029a33aa57b86af785f12d6c786f7c645
marishikaw <ishikawamarina@gmail.com>
2021-10-09 05:49:44 +0000

    [Finish] 3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,8 +3,8 @@ class BlogsController < ApplicationController
     @blogs = Blog.all
   end
 
-
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -14,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blogs_path(blog.id)
   end  
     
   def edit
